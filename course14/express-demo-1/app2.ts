b44ee5b37741d069d984c4e909878b3639f82f92
ergouwang <330035385@qq.com>
2020-06-19 20:13:26 +0800

    install express cli

--- course14/express-demo-1/app2.ts
+++ course14/express-demo-1/app2.ts
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express'
+const app = express();
+const port = 3000;
 
-app.get('/', (request, response) => response.send('Hello World!'))
+app.get('/', (request, response) => response.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
