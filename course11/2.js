7a1cb2f6b6601036d72fe80c70400f1b6f5ebaa7
ergouwang <mrergouwang@gmail.com>
2020-06-19 15:25:27 +0800

    以流的形式使用 execFile

---
+++ course11/2.js
@@ -0,0 +1,12 @@
+const child_process = require('child_process')
+
+const {execFile} = child_process
+
+const userInput = '.'
+
+const streams = execFile("ls",["-la",userInput])
+
+streams.stdout.on('data',(chunk => {
+    console.log(chunk)
+}))
+
