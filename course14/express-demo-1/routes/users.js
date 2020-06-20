e9ef3b14a8293a148099e8b94f37d6ec52ad0846
ergouwang <330035385@qq.com>
2020-06-20 15:34:35 +0800

    项目使用ts编写

--- course14/express-demo-1/routes/users.js
+++ course14/express-demo-1/routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/xxx', function(req, res, next) {
+  res.send('respond users xxx');
+});
 
 module.exports = router;
